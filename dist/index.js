e35f83ac9dedaf0143f4140856da2945c30fb4e7
jdsolanki0001 <jdsolanki0001@gmail.com>
2021-09-27 19:29:35 +0530

    fix(action): build of last commit

--- dist/index.js
+++ dist/index.js
@@ -8431,7 +8431,7 @@ const hasLabel = (issue, label) => {
             // If latest comment is from team member
             if (doesCommentedByMember) {
 
-                if (!doesCommentedByExcludedMember) {
+                if (doesCommentedByExcludedMember) {
                     core.info("Commented by member which is excluded. Exiting.")
                     return
                 }
