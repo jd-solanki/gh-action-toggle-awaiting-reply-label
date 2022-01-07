6b00950239f4299a9a82173b26e646315cc53e8d
JD Solanki <jdsolanki0001@gmail.com>
2022-01-07 15:06:15 +0530

    feat(action): member association option removed
    Breaking change

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# Toggle awaiting-reply/need-more-info label of issue
+# Toggle label of issue on reply
 
 This actions toggle labels like "awaiting-reply" or "need-more-info" which indicates response is required for further action. This is useful if you want to filter out issue already responded to the issue author but the author isn't responded back.
+
+> Note: This is now narrowed down to organization usage only. If you want any other features please raise an issue.
 
 **Use case:**
 Mostly in premium product or service customer support is top priority. In this scenario customer may create issue and sales team replies back withing their business days. This action really helps in filtering only those issues which requires sales teams response. Those issue which are already answered/responded will be marked via "awaiting-reply" or "need-more-info".
 
-This is same of large open source community where an individual what to help people and want to check only those issues where no team member responded yet.
+This is same of large open source community where an individual what to help people and want to check only those issues where no organization member responded yet.
 
 > *This action is highly inspired by already existing action [pending-response](https://github.com/siegerts/pending-response) by [siegerts](https://github.com/siegerts)*
 
@@ -21,7 +23,7 @@ GitHub token
 
 Toggling label. This label will be toggled based on responder. If team member commented then this label will be added and if issue author commented then this label will get removed. According to me best candidate is `awaiting-reply`.
 
-### `member-association`
+<!-- ### `member-association`
 
 #### *Default: OWNER, MEMBER, COLLABORATOR*
 
@@ -29,7 +31,7 @@ Repository associations that are considered part of the team. The action will sk
 
 Please check this [page](https://docs.github.com/en/graphql/reference/enums#commentauthorassociation) for full list of values and detailed information.
 
-> Note: This fields accepts CSV values like: OWNER, MEMBER, COLLABORATOR
+> Note: This fields accepts CSV values like: OWNER, MEMBER, COLLABORATOR -->
 
 ### `ignore-label`
 
@@ -46,6 +48,10 @@ Do note that if `ignore-label` is also found with this label then action will om
 Ignore operation if commented by team members specified in `exclude-members`. This is useful if you want to ignore toggling when bot comment on issue or you already have another action which comment on behalf of you.
 
 > Note: This fields accepts GitHub username CSV like: jd-0001, johnDoe123, tony-robert-stark-007
+
+### `debug`
+
+Boolean value which will tell action to log the action it performs. Useful for debugging.
 
 ## Example Usage
 
@@ -67,25 +73,6 @@ jobs:
         uses: jd-0001/gh-action-toggle-awaiting-reply-label@1.0.1
         with:
           label: awaiting-reply
-```
-
-### Remove specified label only if repo owner or member comment on issue (not collaborator)
-
-```yml
-on:
-  issue_comment:
-    types: [created]
-
-jobs:
-  hello_world_job:
-    runs-on: ubuntu-latest
-    name: Job for toggling label
-    steps:
-      - name: Toggle label
-        uses: jd-0001/gh-action-toggle-awaiting-reply-label@1.0.1
-        with:
-          label: awaiting-reply
-          member-association: OWNER, MEMBER
 ```
 
 ### Ignore toggling label if specified label is present on issue
@@ -126,7 +113,7 @@ jobs:
           only-if-label: support
 ```
 
-### Ignore removing label if mentioned users comment on issue and still present in `member-association`
+### Ignore removing label if mentioned organization member comment on issue
 
 ```yml
 on:
--- dist/index.js
+++ dist/index.js
@@ -8338,130 +8338,162 @@ var __webpack_exports__ = {};
 (() => {
 // Docs: https://docs.github.com/en/actions
 
-const core = __nccwpck_require__(2186);
-const github = __nccwpck_require__(5438);
+const core = __nccwpck_require__(2186)
+const github = __nccwpck_require__(5438)
 
 const getOctokit = () => {
-    const token = core.getInput('token');
+  const token = core.getInput('token')
 
-    if (!token) return core.setFailed("token is required")
+  if (!token) return core.setFailed('token is required')
 
-    return github.getOctokit(token);   
+  return github.getOctokit(token)
 }
 
 const hasLabel = (issue, label) => {
-    const labelNames = issue.labels.map(issueObj => issueObj.name)
-    return labelNames.includes(label)
+  const labelNames = issue.labels.map(issueObj => issueObj.name)
+  return labelNames.includes(label)
 }
 
 (async () => {
-    try {
-    
-        // 👉 Get config
-        const label = core.getInput('label');
-        core.info(`label to toggle: ${label}`)
+  try {
+    // 👉 Get config
+    const debug = Boolean(core.getInput('debug'))
+    core.info(`debug: ${debug}`)
 
-        const ignoreLabel = core.getInput('ignore-label');
-        core.info(`ignoreLabel: ${ignoreLabel}`)
+    const label = core.getInput('label')
+    if (debug) core.info(`label to toggle: ${label}`)
 
-        const onlyIfLabel = core.getInput('only-if-label');
-        core.info(`onlyIfLabel: ${onlyIfLabel}`)
-    
-        const memberAssociation = core.getInput('member-association') || "OWNER, MEMBER, COLLABORATOR"
-        core.info(`Member Association: ${memberAssociation}`)
-    
-        const memberAssociationArray = memberAssociation.split(",").map(a => a.trim())
+    const ignoreLabel = core.getInput('ignore-label')
+    if (debug) core.info(`ignoreLabel: ${ignoreLabel}`)
 
-        const excludeMembers = core.getInput('exclude-members')
-        core.info(`Exclude Members: ${excludeMembers}`)
+    const onlyIfLabel = core.getInput('only-if-label')
+    if (debug) core.info(`onlyIfLabel: ${onlyIfLabel}`)
 
-        const excludeMembersArray = excludeMembers.split(",").map(m => m.trim())
+    const excludeMembers = core.getInput('exclude-members')
+    if (debug) core.info(`Exclude Members: ${excludeMembers}`)
 
-        // 👉 Config Validation
-        if (!label) return core.setFailed("Toggling label is required")
-    
-        // 👉 Get octokit
-        const octokit = getOctokit()
-        const ctx = github.context
-        
-        // console.log(`ctx.eventName: ${ctx.eventName}`)
-        // console.log(`Payload: ${JSON.stringify(ctx.payload, undefined, 2)}`)
-    
-        // Docs: https://docs.github.com/en/actions/learn-github-actions/events-that-trigger-workflows
-    
-        // Add constraint => Run only if new comment is posted on issue && issue is raised support
-        if (ctx.eventName === "issue_comment" && ctx.payload.action === "created") {
+    const excludeMembersArray = excludeMembers.split(',').map(m => m.trim())
 
-            // If `onlyIfLabel` label is provided & that label is not present on issue => ignore
-            if (onlyIfLabel && !hasLabel(ctx.payload.issue, onlyIfLabel)) {
-                core.info(`Ignoring as onlyIfLabel "${onlyIfLabel}" is not present on issue. Exiting.`)
-                return
-            }
+    // 👉 Config Validation
+    if (!label) return core.setFailed('Toggling label is required')
 
-            // If `ignoreLabel` label is provided & that label is present on issue => ignore
-            if (ignoreLabel && hasLabel(ctx.payload.issue, ignoreLabel)) {
-                core.info(`Ignoring as ignoreLabel "${ignoreLabel}" is present on issue. Exiting.`)
-                return
-            }
-    
-            // Fetch the issue
-            const { data: issue } = await octokit.rest.issues.get({
-                issue_number: ctx.payload.issue.number,
-                owner: ctx.repo.owner,
-                repo: ctx.repo.repo,
-            })
+    // 👉 Get octokit
+    const octokit = getOctokit()
+    const ctx = github.context
 
-            // console.log("Issue:", JSON.stringify(issue, undefined, 2))
-            
-            // If issue is closed => return
-            if (issue.state === "closed") return
+    if (debug) core.info(`ctx.eventName: ${ctx.eventName}`)
+    if (debug) core.info(`Payload: ${JSON.stringify(ctx.payload, undefined, 2)}`)
 
-            core.info(`Issue state: ${issue.state}`)
-            core.info(`Issue title: ${issue.title}`)
-            
-            // Grab Latest comment
-            const doesCommentedByMember = memberAssociationArray.includes(ctx.payload.comment.author_association)
-            const doesCommentedByAuthor = ctx.payload.comment.user.id === ctx.payload.issue.user.id
-            const doesCommentedByExcludedMember = excludeMembersArray.includes(ctx.payload.comment.user.login)
+    // Docs: https://docs.github.com/en/actions/learn-github-actions/events-that-trigger-workflows
 
-            core.info(`Does commented by author: ${doesCommentedByAuthor}`)
-            core.info(`Does commented by member: ${doesCommentedByMember}`)
-            core.info(`Does commented by member which is excluded: ${doesCommentedByExcludedMember}`)
-            
-            // If latest comment is from team member
-            if (doesCommentedByMember) {
+    // Add constraint => Run only if new comment is posted on issue && issue is raised support
+    if (ctx.eventName === 'issue_comment' && ctx.payload.action === 'created') {
+      // If `onlyIfLabel` label is provided & that label is not present on issue => ignore
+      if (onlyIfLabel && !hasLabel(ctx.payload.issue, onlyIfLabel)) {
+        core.info(`Ignoring as onlyIfLabel "${onlyIfLabel}" is not present on issue. Exiting.`)
+        return null
+      }
 
-                if (doesCommentedByExcludedMember) {
-                    core.info("Commented by member which is excluded. Exiting.")
-                    return
-                }
+      // If `ignoreLabel` label is provided & that label is present on issue => ignore
+      if (ignoreLabel && hasLabel(ctx.payload.issue, ignoreLabel)) {
+        core.info(`Ignoring as ignoreLabel "${ignoreLabel}" is present on issue. Exiting.`)
+        return null
+      }
 
-                // Apply label
-                octokit.rest.issues.addLabels({
-                    issue_number: ctx.payload.issue.number,
-                    owner: ctx.repo.owner,
-                    repo: ctx.repo.repo,
-                    labels: [label]
-                })
-            } else if (doesCommentedByAuthor) {
-                // If latest comment is from author
-                // Remove label
-                octokit.rest.issues.removeLabel({
-                    issue_number: ctx.payload.issue.number,
-                    owner: ctx.repo.owner,
-                    repo: ctx.repo.repo,
-                    name: label
-                })
-            } else {
-                core.info("Commented by some other user. Exiting.")
-            }
+      if (debug) core.info('Fetching issue for more details')
 
+      // Fetch the issue
+      const { data: issue } = await octokit.rest.issues.get({
+        issue_number: ctx.payload.issue.number,
+        owner: ctx.repo.owner,
+        repo: ctx.repo.repo,
+      })
+
+      if (debug) core.info('Issue:', JSON.stringify(issue, undefined, 2))
+
+      // If issue is closed => return
+      if (issue.state === 'closed') {
+        if (debug) core.info('Issue is closed. Existing.')
+        return null
+      }
+
+      if (debug) core.info(`Issue state: ${issue.state}`)
+      if (debug) core.info(`Issue title: ${issue.title}`)
+
+      let doesCommentedByMember = false
+
+      try {
+        if (debug) core.info('Making request to check if comment is made by org member')
+
+        // ℹ️ Check if user is organization member
+        // https://docs.github.com/en/rest/reference/orgs#check-organization-membership-for-a-user
+        const { status } = await octokit.rest.orgs.checkMembershipForUser({
+          org: ctx.repo.owner,
+          username: ctx.payload.comment.user.login,
+        })
+
+        /*
+                status ==== 204 => org member
+                status ==== 302 => requester is not member
+                status ==== 404 => requester is member and user is not member
+              */
+
+        if (debug) core.info(`'Request response status: ${status}'`)
+
+        if (status === 204) doesCommentedByMember = true
+      } catch (error) {
+        if (debug) {
+          core.info('Request threw exception. Handled in catch block.')
+          core.info(`'Error response status: ${error.response.status}'`)
+          core.info('Error:')
+          // core.info(error.response)
+          core.info(error.response.data.message)
+          core.info('Got 404 as  request response status. Hence, user is not org member')
         }
-    
-    } catch (error) {
-      core.setFailed(error.message);
+      }
+
+      // Grab Latest comment
+      const doesCommentedByAuthor = ctx.payload.comment.user.id === ctx.payload.issue.user.id
+      const doesCommentedByExcludedMember = excludeMembersArray.includes(ctx.payload.comment.user.login)
+
+      core.info(`Does commented by author: ${doesCommentedByAuthor}`)
+      core.info(`Does commented by member: ${doesCommentedByMember}`)
+      core.info(`Does commented by member which is excluded: ${doesCommentedByExcludedMember}`)
+
+      // If latest comment is from team member
+      if (doesCommentedByMember) {
+        if (doesCommentedByExcludedMember) {
+          core.info('Commented by member which is excluded. Exiting.')
+          return null
+        }
+
+        // Apply label
+        octokit.rest.issues.addLabels({
+          issue_number: ctx.payload.issue.number,
+          owner: ctx.repo.owner,
+          repo: ctx.repo.repo,
+          labels: [label],
+        })
+      } else if (doesCommentedByAuthor) {
+        // If latest comment is from author
+        // Remove label
+        octokit.rest.issues.removeLabel({
+          issue_number: ctx.payload.issue.number,
+          owner: ctx.repo.owner,
+          repo: ctx.repo.repo,
+          name: label,
+        })
+      } else {
+        core.info('Commented by some other user. Exiting.')
+      }
     }
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+
+  return null
 })()
+
 })();
 
 module.exports = __webpack_exports__;
